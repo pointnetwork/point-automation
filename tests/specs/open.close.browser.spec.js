fbc94111de02cd969d0bd415c4455f0246f96402
Alexis Alvarez <alexis@pointlabs.org>
2022-08-25 18:04:43 -0300

    Fix

--- tests/specs/open.close.browser.spec.js
+++ tests/specs/open.close.browser.spec.js
@@ -14,6 +14,7 @@ describe('Open/Close Browser', () => {
         await InstallerWelcomePage.waitForInstallerToBeDisplayed();
         await InstallerWelcomePage.clickOnStartInstallationButton();
         await InstallerWelcomePage.waitForInstallationCompleted();
+        await browser.reloadSession();
 
         await LoginPage.waitForPageToBeLoaded();
         expect(LoginPage.noGenerateOneButton.chromeBrowser).toBeDisplayed();
