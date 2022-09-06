9d065fabed0458732fe015cb098ae1a2df152da5
Alexis Alvarez <alexis@pointlabs.org>
2022-09-06 09:47:57 -0300

    Fix linux

--- tests/config/config-github-actions-linux.conf.js
+++ tests/config/config-github-actions-linux.conf.js
@@ -6,8 +6,8 @@ config.capabilities =
       capabilities: {
         browserName: 'chrome',
         'goog:chromeOptions': {
-          binary: '/Users/runner/work/point-automation/point-automation/point.app/Contents/MacOS/point',
-          args: ["app='/Users/runner/work/point-automation/point-automation/point.app/Contents/MacOS/point'"]
+            binary: '/usr/bin/point',
+            args: ["app='/usr/bin/point'"]
         }
       }
     }
