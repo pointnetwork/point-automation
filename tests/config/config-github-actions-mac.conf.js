55b1da0e72ab51419dba34d59ca52678fa39d105
Alexis Alvarez <alexis@pointlabs.org>
2022-09-06 14:53:44 -0300

    Fix mac

--- tests/config/config-github-actions-mac.conf.js
+++ tests/config/config-github-actions-mac.conf.js
@@ -1,16 +1,15 @@
 const { config } = require('./config.conf');
 
 config.capabilities =
-    {
-        chromeBrowser: {
-            capabilities: {
-                browserName: 'chrome',
-                'goog:chromeOptions': {
-                    binary: '/usr/bin/point',
-                    args: ["app='/usr/bin/point'"]
-                }
-            }
+  {
+    chromeBrowser: {
+      capabilities: {
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+          binary: '/Users/runner/work/point-automation/point-automation/point.app/Contents/MacOS/point',
+          args: ["app='/Users/runner/work/point-automation/point-automation/point.app/Contents/MacOS/point'"]
         }
+      }
     }
-
+  }
 exports.config = config;
