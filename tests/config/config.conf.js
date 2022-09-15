a419babbf58518b8be62d7f6457d26cd00cb7da4
Alexis Alvarez <alexis@pointlabs.org>
2022-09-15 15:41:29 -0300

    Removed tests

--- tests/config/config.conf.js
+++ tests/config/config.conf.js
@@ -233,8 +233,9 @@ exports.config = {
             './tests/specs/logout.existing.key.spec.js',
             './tests/specs/logout.generate.new.key.not.closing.browser.spec.js',
             './tests/specs/logout.generate.new.key.spec.js',
-            './tests/specs/open.close.dashboard.spec.js',
-            './tests/specs/remove.point.folder.login.test.spec.js',
+            // './tests/specs/open.close.dashboard.spec.js',
+            // './tests/specs/remove.point.folder.login.test.spec.js',
+            // './tests/specs/remove.point.folder.signup.test.spec.js',
         ],
     },
 };
