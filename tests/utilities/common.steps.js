dfc24bdc9405a9503d22459e30ef380d3a604376
Alexis Alvarez <alexis@pointlabs.org>
2022-09-27 11:38:47 -0300

    Fix

--- tests/utilities/common.steps.js
+++ tests/utilities/common.steps.js
@@ -19,7 +19,7 @@ module.exports = {
     async loginUser(processes=3) {
         await this.installAppIfIsRequired()
         await console.log("Logging in user...")
-        await Utils.reloadSessionLinux()
+        // await Utils.reloadSessionLinux()
         // if(process.platform === "linux") {
         //      await browser.pause(5000)
         //      await browser.reloadSession()
