c4557c27a3d8b6e80d3a2a9777541389b681c8b1
Alexis Alvarez <alexis@pointlabs.org>
2022-09-27 15:38:54 -0300

    Expect

--- tests/specs/open.close.browser.spec.js
+++ tests/specs/open.close.browser.spec.js
@@ -24,7 +24,7 @@ describe('Open/Close Browser', () => {
             expect((await DashboardPage.launchPointBrowserButton).chromeBrowser).toBeDisplayed();
             await DashboardPage.clickOnLogout()
             await DashboardPage.confirmLogout();
-            assert(true).toEqual(false)
+            expect(true).toEqual(false)
             attempts -= 1;
             await console.log("Times to run : " + attempts);
         }
