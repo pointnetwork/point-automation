534863f382d4207d5195496062223a5ef14ba16c
Alexis Alvarez <alexis@pointlabs.org>
2022-09-29 17:44:16 -0300

    Fix logout

--- tests/pages/login.page.js
+++ tests/pages/login.page.js
@@ -1,4 +1,5 @@
 import Page from './page'
+import DashboardPage from "./dashboard.page";
 
 class LoginPage extends Page {
     get noGenerateOneButton() {
@@ -16,7 +17,13 @@ class LoginPage extends Page {
 
     async waitForLoginPage() {
         await console.log("Checking login page...")
-        await (await this.noGenerateOneButton).chromeBrowser.waitForDisplayed({timeout: 7000});
+        try {
+            await (await this.noGenerateOneButton).chromeBrowser.waitForDisplayed({timeout: 7000});
+        }catch(exception) {
+            await console.log("User is still logged in. Logging out now...")
+            await DashboardPage.clickOnLogout()
+            await DashboardPage.confirmLogout();
+        }
     }
 
     async clickOnNoGenerateOne() {
