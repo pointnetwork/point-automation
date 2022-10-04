ab7286dbfa547cb7b6653e307f246a33a3bac67b
Alexis Alvarez <alexis@pointlabs.org>
2022-10-04 11:23:45 -0300

    Reorder point linux

--- tests/config/config.conf.js
+++ tests/config/config.conf.js
@@ -245,11 +245,11 @@ exports.config = {
             './tests/specs/verify.browser.page.spec.js',
         ],
         github_actions_linux: [
-            './tests/specs/remove.point.folder.login.test.spec.js',
-            './tests/specs/remove.point.folder.signup.test.spec.js',
             './tests/specs/logout.existing.key.spec.js',
             './tests/specs/logout.generate.new.key.not.closing.browser.spec.js',
             './tests/specs/logout.generate.new.key.spec.js',
+            './tests/specs/remove.point.folder.login.test.spec.js',
+            './tests/specs/remove.point.folder.signup.test.spec.js',
             './tests/specs/open.close.browser.spec.js',
         ]
     },
