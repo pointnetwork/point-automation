750cb88926643e9503fd4b2286816ac3ac8be146
Alexis Alvarez <alexis@pointlabs.org>
2022-10-18 14:01:03 -0300

    Firefox tests added

--- tests/config/config.conf.js
+++ tests/config/config.conf.js
@@ -247,13 +247,13 @@ exports.config = {
         ],
         github_actions_linux: [
             './tests/specs/logout.existing.key.spec.js',
-            // './tests/specs/logout.generate.new.key.not.closing.browser.spec.js',
             './tests/specs/logout.generate.new.key.spec.js',
             './tests/specs/remove.point.folder.login.test.spec.js',
             './tests/specs/remove.point.folder.signup.test.spec.js',
             './tests/specs/open.close.browser.spec.js',
-            // './tests/specs/verify.browser.page.spec.js',
-            // './tests/specs/generate.new.key.new.identity.browser.spec.js',
+            './tests/specs/verify.browser.page.spec.js',
+            './tests/specs/generate.new.key.new.identity.browser.spec.js',
+            './tests/specs/logout.generate.new.key.not.closing.browser.spec.js',
         ]
     },
 };
