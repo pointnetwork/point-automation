26198e94c0e6eba42d10914340f98a5c365851ce
Alexis Alvarez <alexis@pointlabs.org>
2022-10-20 18:38:41 -0300

    Pageobjects finished

---
+++ tests/pages/browser/browser.identity.page.js
@@ -0,0 +1,72 @@
+import Page from '../page'
+
+export default class BrowserIdentityPage extends Page {
+    constructor(firefox) {
+        super();
+        this.driver = firefox
+    }
+
+    get identityTitle() {
+        return this.driver.$('//h1')
+    }
+
+    get handleValue() {
+        return this.driver.$('//th[contains(text(), \'Handle\')]/following-sibling::td')
+    }
+
+    get ownerValue() {
+        return this.driver.$('//th[contains(text(), \'Owner\')]/following-sibling::td')
+    }
+
+    get communicationPublicKeyValue() {
+        return this.driver.$('//th[contains(text(), \'Communication Public Key\')]/following-sibling::td')
+    }
+
+    get domainSpaceValue() {
+        return this.driver.$('//th[contains(text(), \'Domain Space\')]/following-sibling::td')
+    }
+
+    get addNewEntryKeyTextbox() {
+        return this.driver.$("//input[@name='ikvEntryKey']")
+    }
+
+    get addNewEntryValueTextbox() {
+        return this.driver.$("//input[@name='ikvEntryValueRef']")
+    }
+
+    get addNewEntryVersionTextbox() {
+        return this.driver.$("//input[@name='ikvEntryVersion']")
+    }
+
+    get addNewEntryAddButton() {
+        return this.driver.$("//h5[text() = 'Add new entry']/..//button[text() = 'Add']")
+    }
+
+    get deployersAddressTextbox() {
+        return this.driver.$("//input[@name='addAddress']")
+    }
+
+    get addDeployerAddButton() {
+        return this.driver.$("//h5[text() = 'Deployers']/..//button[text() = 'Add']")
+    }
+
+    async waitForPageToBeLoaded() {
+        await this.handleValue.waitForDisplayed()
+    }
+
+    async clickOnDomainSpace() {
+        await super.clickElement(this.domainSpaceValue)
+    }
+
+    async addNewEntry(key, value, version) {
+        await super.setValueInElement(this.addNewEntryKeyTextbox, key)
+        await super.setValueInElement(this.addNewEntryValueTextbox, value)
+        await super.setValueInElement(this.addNewEntryVersionTextbox, version)
+        await super.clickElement(this.addNewEntryAddButton)
+    }
+
+    async addNewDeployer(address) {
+        await super.setValueInElement(this.deployersAddressTextbox, address)
+        await super.clickElement(this.addDeployerAddButton)
+    }
+}
---
+++ tests/pages/browser/browser.my.identities.page.js
@@ -0,0 +1,44 @@
+import Page from '../page'
+
+export default class BrowserMyIdentitiesPage extends Page {
+    constructor(firefox) {
+        super();
+        this.driver = firefox
+    }
+
+    get myIdentitiesTitle() {
+        return this.driver.$('//h1')
+    }
+
+    get handleLink() {
+        return this.driver.$('//td/a[contains(@href, \'/identities/\')]')
+    }
+
+    get handleOwner() {
+        return this.driver.$('//td[2]')
+    }
+
+    get subIdentityTextbox() {
+        return this.driver.$('//input[@name=\'register-subidentity\']')
+    }
+
+    get registerButton() {
+        return this.driver.$('//button[contains(text(), \'Register\')]')
+    }
+
+    async waitForPageToBeLoaded() {
+        await this.handleLink().waitForDisplayed();
+    }
+
+    async clickOnHandle() {
+        await super.clickElement(this.handleLink)
+    }
+
+    async enterNewSubIdentity(name){
+        await super.setValueInElement(this.subIdentityTextbox, name)
+    }
+
+    async clickOnRegister() {
+        await super.clickElement(this.registerButton)
+    }
+}
