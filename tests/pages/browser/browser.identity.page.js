fac86fc5c1b1ef03af50120a2ce1ce3ae0b1fd9d
Alexis Alvarez <alexis@pointlabs.org>
2022-10-26 17:58:33 -0300

    New test to add deployer ready

--- tests/pages/browser/browser.identity.page.js
+++ tests/pages/browser/browser.identity.page.js
@@ -1,4 +1,5 @@
 import Page from '../page'
+import BrowserTransactionModalPage from "./browser.transaction.modal.page";
 
 export default class BrowserIdentityPage extends Page {
     constructor(firefox) {
@@ -54,6 +55,26 @@ export default class BrowserIdentityPage extends Page {
         return this.driver.$$("(//table)[2]//tbody//tr")
     }
 
+    get deployersTable() {
+        return this.driver.$("(//table)[3]")
+    }
+
+    get revokeDeployerButton() {
+        return this.driver.$(".btn.btn-sm.btn-danger")
+    }
+
+    get firstDeployerRow() {
+        return this.driver.$("(//table)[3]//tbody//tr[1]")
+    }
+
+    get successDeployerAddedMessage() {
+        return this.driver.$("//div[contains(@class, 'swal2-popup')]")
+    }
+
+    get successDeployerOkButton() {
+        return this.driver.$("//div[contains(@class, 'swal2-popup')]//button[contains(text(), 'OK')]")
+    }
+
     async getIkvNameByRowIndex(index) {
         return this.driver.$("(//table)[2]//tbody//tr["+index+"]//td[1]")
     }
@@ -82,6 +103,26 @@ export default class BrowserIdentityPage extends Page {
         return this.driver.$("(//table)[2]//tbody//tr["+index+"]//td[4]//button[2]")
     }
 
+    async getDeployerNameByRowIndex(index) {
+        return this.driver.$("(//table)[3]//tbody//tr["+index+"]//th[1]")
+    }
+
+    async getDeployerAddressByRowIndex(index) {
+        return this.driver.$("(//table)[3]//tbody//tr["+index+"]//td[1]")
+    }
+
+    async getDeployerStatusByRowIndex(index) {
+        return this.driver.$("(//table)[3]//tbody//tr["+index+"]//td[2]")
+    }
+
+    async getDeployerDateByRowIndex(index) {
+        return this.driver.$("(//table)[3]//tbody//tr["+index+"]//td[3]")
+    }
+
+    async getDeployerReactiveButtonByRowIndex(index) {
+        return this.driver.$("(//table)[3]//tbody//tr["+index+"]//td[4]//button")
+    }
+
     async waitForPageToBeLoaded() {
         await this.identityTitle.waitForDisplayed()
     }
@@ -99,6 +140,9 @@ export default class BrowserIdentityPage extends Page {
     }
 
     async addNewDeployer(address) {
+        await browser.pause(5000)
+        await this.addDeployerAddButton.scrollIntoView()
+        await browser.pause(2000)
         await super.setValueInElement(this.deployersAddressTextbox, address)
         await super.clickElement(this.addDeployerAddButton)
     }
@@ -122,5 +166,54 @@ export default class BrowserIdentityPage extends Page {
         await browser.pause(5000)
         await this.addDeployerAddButton.scrollIntoView()
         await browser.pause(2000)
+    }
+
+    async moveToFirstDeployer() {
+        await browser.pause(5000)
+        await this.firstDeployerRow.scrollIntoView()
+        await browser.pause(2000)
+    }
+
+    async revokeDeployerIfDisplayed() {
+        try {
+            await this.moveToLastIkvRow();
+            await this.revokeDeployerButton.waitForDisplayed({timeout:5000})
+            if(await this.revokeDeployerButton.isDisplayed()) {
+                await super.clickElement(this.revokeDeployerButton);
+                const browserTransactionModalPage = await new BrowserTransactionModalPage(this.driver)
+                await this.switchToTab("Point Confirmation Window", this.driver)
+                await browserTransactionModalPage.clickOnAllow()
+                await browserTransactionModalPage.switchToTab("Point Explorer", this.driver)
+                await browserTransactionModalPage.waitForSpinnerNotDisplayed(this.driver)
+                await this.driver.refresh()
+                await this.waitForPageToBeLoaded()
+                return "Revoked"
+            }
+        }catch(exception) {
+            console.log("No deployer was found to remove")
+            return "Allowed"
+        }
+    }
+
+    async clickOnReactivateButtonByIndex(index) {
+        await super.clickElement(await this.getDeployerReactiveButtonByRowIndex(index))
+    }
+
+    async clickOnReactivateButtonByIndexIfNeeded(status, number) {
+        if(status === "Revoked") {
+            await this.clickOnReactivateButtonByIndex(number)
+            const browserTransactionModalPage = await new BrowserTransactionModalPage(this.driver)
+            await this.switchToTab("Point Confirmation Window", this.driver)
+            await browserTransactionModalPage.clickOnAllow()
+            await browserTransactionModalPage.switchToTab("Point Explorer", this.driver)
+            await browserTransactionModalPage.waitForSpinnerNotDisplayed(this.driver)
+            await this.driver.refresh()
+            await this.waitForPageToBeLoaded()
+            await this.deployersTable.waitForDisplayed()
+        }
+    }
+
+    async clickOnDeployerAddedOkButton() {
+        await super.clickElement(await this.successDeployerOkButton)
     }
 }
---
+++ tests/specs/identity.add.deployer.browser.spec.js
@@ -0,0 +1,79 @@
+import DashboardPage from '../pages/dashboard.page'
+
+import CommonSteps from "../utilities/common.steps";
+import BashProcesses from "../utilities/bash.processes";
+import BrowserTopBarPage from "../pages/browser/browser.top.bar.page";
+import BrowserIdentityPage from "../pages/browser/browser.identity.page";
+import {faker} from "@faker-js/faker";
+import BrowserTransactionModalPage from "../pages/browser/browser.transaction.modal.page";
+import Credentials from "../resources/decryptedcredentials.json";
+
+describe('Identity / Deployer', () => {
+    it('Validate a Deployer can be added correctly', async () => {
+        //Login
+        await CommonSteps.loginIfUserIsLoggedOut();
+        await DashboardPage.waitForDashboardDisplayed();
+        await DashboardPage.waitForProcessesRunning();
+
+        //Kill firefox
+        await DashboardPage.waitForDashboardDisplayed();
+        await DashboardPage.changeToActiveWindow();
+        await DashboardPage.waitForProcessesRunning();
+        await BashProcesses.killAllFirefoxProcesses();
+        await DashboardPage.waitForProcessesRunning(1);
+        await browser.pause(10000)
+        await DashboardPage.launchPointBrowserButton.waitForDisplayed();
+        expect(await DashboardPage.launchPointBrowserButton).toBeDisplayed();
+
+        //Open firefox instance
+        const firefox = await CommonSteps.createFirefoxInstance()
+        await CommonSteps.openPointInNewFirefox(firefox);
+
+        //Enter to personal identity
+        const browserTopBarPage = await new BrowserTopBarPage(firefox)
+        await browserTopBarPage.clickOnIdentityButton()
+        const browserIdentityPage = await new BrowserIdentityPage(firefox)
+        await browserIdentityPage.waitForPageToBeLoaded()
+
+        //Revoke deployer
+        const expectedStatus = await browserIdentityPage.revokeDeployerIfDisplayed()
+        const browserTransactionModalPage = await new BrowserTransactionModalPage(firefox)
+
+        //Add new deployer
+        const address = Credentials.accountSecondUser
+        await browserIdentityPage.addNewDeployer(address)
+        await browserIdentityPage.switchToTab("Point Confirmation Window", firefox)
+        await browserTransactionModalPage.clickOnAllow()
+        await browserTransactionModalPage.switchToTab("Point Explorer", firefox)
+        await browserTransactionModalPage.waitForSpinnerNotDisplayed(firefox)
+        expect(await browserIdentityPage.successDeployerAddedMessage).toBeDisplayed()
+        expect(await browserIdentityPage.successDeployerOkButton).toBeDisplayed()
+        await browserIdentityPage.clickOnDeployerAddedOkButton()
+
+        await firefox.refresh()
+        await browserIdentityPage.waitForPageToBeLoaded()
+        await browserIdentityPage.deployersTable.waitForDisplayed()
+        await browserIdentityPage.moveToFirstDeployer()
+
+        //Assertions on Deployers table
+        expect(await browserIdentityPage.getDeployerNameByRowIndex(1)).toHaveText("@alexistesttwo")
+        expect(await browserIdentityPage.getDeployerAddressByRowIndex(1)).toHaveText(address)
+        expect(await browserIdentityPage.getDeployerStatusByRowIndex(1)).toHaveText(expectedStatus)
+        expect(await browserIdentityPage.getDeployerDateByRowIndex(1)).toBeDisplayed()
+
+        //Reactivate
+        await browserIdentityPage.clickOnReactivateButtonByIndexIfNeeded(expectedStatus, 1)
+
+        await firefox.refresh()
+        await browserIdentityPage.waitForPageToBeLoaded()
+        await browserIdentityPage.deployersTable.waitForDisplayed()
+        await browserIdentityPage.moveToFirstDeployer()
+
+        expect(await browserIdentityPage.getDeployerNameByRowIndex(1)).toHaveText("@alexistesttwo")
+        expect(await browserIdentityPage.getDeployerAddressByRowIndex(1)).toHaveText(address)
+        expect(await browserIdentityPage.getDeployerStatusByRowIndex(1)).toHaveText("Allowed")
+        expect(await browserIdentityPage.getDeployerDateByRowIndex(1)).toBeDisplayed()
+        expect(await browserIdentityPage.getDeployerReactiveButtonByRowIndex(1)).toHaveText("Revoke")
+        await BashProcesses.killAllFirefoxProcesses();
+    });
+});
